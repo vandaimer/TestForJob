ed5159f3ad7aeb93f240ab76ea1179a1018a8fc3
Luiz Filipe <moresco.luiz@gmail.co>
2016-01-07 15:57:31 -0200

    Commit inicial

---
+++ desafio1.php
@@ -0,0 +1,21 @@
+<form action="desafio1.php" method="post">
+	Dividendo: <input type="text" name="dividendo">
+	<br/>
+	Divisor: <input type="text" name="divisor">
+	<br/>
+	<input type="submit">
+</form>
+
+<?php
+if( !empty( $_POST ) )
+{
+	$dividendo = (int)$_POST['dividendo'];
+	$divisor   = (int)$_POST['divisor'];
+	if( $divisor === 0 )
+	{
+		echo "Digite um número diferente de zero para no campo divisor";
+		return;
+	}
+	$resultado = $dividendo / $divisor;
+	echo "Resultado: {$resultado}.";
+}
---
+++ desafio2.php
@@ -0,0 +1,32 @@
+Primeiro caracter único de uma string<br/>
+<form action="desafio2.php" method="post">
+Palavra: <input type='text' name="palavra">
+<br/>
+<input type='submit' value="Qual é o caracter?">
+</form>
+<?php
+if( !empty( $_POST ) )
+{
+	$palavra = $_POST['palavra'];
+
+	if( empty( $palavra ) )
+	{
+		echo "Palavra vazia.";
+		return;
+	}
+
+	if( strlen( $palavra ) === 1 )
+	{
+		echo $palavra;
+		return;
+	}
+
+	foreach( str_split( $palavra ) as $letra )
+	{
+		if( substr_count( $palavra, $letra ) === 1)
+		{
+			echo "Primeiro caracter único: {$letra}";
+			break;
+		}
+	}
+}
